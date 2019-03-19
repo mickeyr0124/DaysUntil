1b289c3290a208c439144ec00ffd884ae5926647
Mickey <mickey@rose-trees.com>
2019-03-19 15:53:43 -0400

    Uses Google Drive

---
+++ DaysUntil/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.Color.Red
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(665, 220)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(689, 244)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Days Until"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ DaysUntil/Form1.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Xml
+Imports System.Data
+Imports System.IO
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.DataGridView1.EnableHeadersVisualStyles = False
+        Me.DataGridView1.Font = New Font("ComicSans", 20, FontStyle.Regular)
+        Me.DataGridView1.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.Red
+
+
+        Dim filePath As String = Environ("USERPROFILE") & "\Google Drive\"
+        If Directory.Exists(filePath) Then
+            'Program may proceed
+            Dim xmlFile As XmlReader
+            xmlFile = XmlReader.Create(filePath & "\Travel\DaysUntil.xml", New XmlReaderSettings())
+            Dim ds As New DataSet
+            ds.ReadXml(xmlFile)
+            ds.Tables(0).Columns.Add("Days Until")
+            For Each r As DataRow In ds.Tables(0).Rows
+                Dim d As Date = r("Date")
+                Dim n As Integer = d.Subtract(Today).Days
+                r("Days Until") = n
+            Next
+            DataGridView1.DataSource = ds.Tables(0)
+            For i As Integer = 0 To Me.DataGridView1.Rows.Count - 1
+                If Me.DataGridView1.Rows(i).Cells("Days Until").Value < 30 Then
+                    For j = 0 To Me.DataGridView1.ColumnCount - 1
+                        Me.DataGridView1.Rows(i).Cells(j).Style.ForeColor = Color.Red
+                    Next
+                End If
+            Next
+        Else
+            MsgBox("You must install Dropbox on this computer in order for this application to function properly.")
+            End
+        End If
+
+    End Sub
+
+    Private Sub DataGridView1_SelectionChanged(sender As Object, e As EventArgs) Handles DataGridView1.SelectionChanged
+        Me.DataGridView1.ClearSelection()
+    End Sub
+End Class
